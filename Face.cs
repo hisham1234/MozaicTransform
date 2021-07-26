7c33af9e633cef2390fbeb9072fb2a30c4799c67
Lucien Senaneuch <senaneuch.lucien@enexcosmh.onmicrosoft.com>
2021-07-26 12:05:35 +0900

    [Logs] Refactoring logs in order to match the documentation specification

--- Face.cs
+++ Face.cs
@@ -66,7 +66,7 @@ namespace MozaicTransform
                 
                 blobClient.Upload(uploadStream, true);
                 
-                log.LogInformation(DateTime.Now.ToString("| Mozaic-Transform | Picture :" + name + " blured uploaded successfully!");
+                log.LogInformation(DateTime.Now.ToString("| Mozaic-Transform | Picture :" + name + " blured uploaded successfully!"));
             }
             catch (Exception ex)
             {
